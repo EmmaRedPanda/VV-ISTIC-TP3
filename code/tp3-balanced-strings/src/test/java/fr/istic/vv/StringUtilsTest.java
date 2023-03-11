4e2d0297bfd6a90b4a89b629365b0edfc4d86206
Emma <91788047+EmmaRedPanda@users.noreply.github.com>
2023-03-11 21:09:22 +0100

    Update StringUtilsTest.java

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -6,6 +6,27 @@ import static fr.istic.vv.StringUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
-
+  
+  @org.junit.jupiter.api.Test
+    void monTest(){
+        assertTrue(isBalanced(""));
+        assertTrue(isBalanced("Hello"));
+        assertTrue(isBalanced("This is a [test]!"));
+        assertTrue(isBalanced("()"));
+        assertTrue(isBalanced("{}"));
+        assertTrue(isBalanced("[]"));
+        assertFalse(isBalanced("("));
+        assertFalse(isBalanced(")"));
+        assertFalse(isBalanced("{"));
+        assertFalse(isBalanced("}"));
+        assertFalse(isBalanced("["));
+        assertFalse(isBalanced("]"));
+        assertFalse(isBalanced("()("));
+        assertFalse(isBalanced("[[[[[["));
+        assertFalse(isBalanced("(]"));
+        assertFalse(isBalanced("{[}]"));
+        assertTrue(isBalanced("{ [ ( ) ] }"));
+        assertTrue(isBalanced("[{}](){}"));
+    }
 
 }
